7a830715019be3c4a3b49567c0fa633e331d92d0
Aditya Rizqiqa Ramadhan <rizqiqaaditya@gmail.com>
2026-02-25 13:23:44 +0700

    Jobsheet 2_Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return 'Hello World';
 });
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/selamat', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/Mahasiswa', function () {
+    return 'Nama : Aditya Rizqiqa Ramadhan <br> NIM : 244107020008';
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya, ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke- ' . $postId . ' - Komentar ke-: ' . $commentId;
+});
+
+Route::get('/articles/{id}', function ($articlesId) {
+    return 'Halaman Artikel dengan ID ' . $articlesId;
+});
+
